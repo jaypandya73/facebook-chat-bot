637b8d45478900400c4e657f18fb44bae4da12e5
jay-pandya-btc <jay.pandya@botreetechnologies.com>
2016-05-15 14:51:14 +0530

    change

--- config/initializers/messanger_bot.rb
+++ config/initializers/messanger_bot.rb
@@ -1,5 +1,8 @@
 Messenger::Bot.config do |config|
-  config.access_token = 'EAAGE6upBuisBAC0fUnYxsb7rtR2QAfSRUtxdoFbenJ05VkZBPMBXQos9Q26wt2XL9W4kGlosLmpYfeUpigF5McIi1Dm5S0ZCQpZCcN23XH0Yye3hidiyMafQDvxbUNzSXXJpVfbZBjKxmX7DqCM6y9meNEdykW9gHzXGgPAlMAZDZD'
+  config.access_token = 'EAAGE6upBuisBABdikqNINoK0twYRYimZBWrUoiZBlVBFbv7Se3WGi40gmoJjnTAjl8ZCvWEbgFA9BFoikynft6yjvMPHgfhoz21EEsHy5P6FdmdZANt2TtRldZA5QBnlX7zTyWh1eL9dcXB9ZCK4vUK78WE58PwGeSsQU6xHWQNwZDZD'
   config.validation_token = 'total_secret'
   config.secret_token = '8a81891bf89a8b71b5ba70f0bc201896'
 end
+
+
+
