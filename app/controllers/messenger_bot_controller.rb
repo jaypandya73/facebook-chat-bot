0efa5ed1cfe1f35a3c0c9215ba17c25e46416adf
jay-pandya-btc <jay.pandya@botreetechnologies.com>
2016-05-15 18:19:59 +0530

    logs

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -2,13 +2,16 @@ class MessengerBotController < ActionController::Base
   def message(event, sender)
     profile = sender.get_profile(:first_name) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
     puts "I am here"
+    puts "#{event}"
     sender.reply({ text: "#{event['message']['text']}" })
   end
 
   def delivery(event, sender)
+    puts "I am in delivery"
   end
 
   def postback(event, sender)
+    puts "I am in postback"
     payload = event["postback"]["payload"]
     case payload
     when :something
