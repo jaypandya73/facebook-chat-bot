4d48637ec1855b3d703c76b5d165df186c9294e2
jay-pandya-btc <jay.pandya@botreetechnologies.com>
2016-05-15 17:57:47 +0530

    controller change

--- app/controllers/messenger_bot_controller.rb
+++ app/controllers/messenger_bot_controller.rb
@@ -1,7 +1,6 @@
 class MessengerBotController < ActionController::Base
   def message(event, sender)
-    profile = sender.get_profile(first_name) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
-    puts "#{sender}"
+    # profile = sender.get_profile(first_name) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
     puts "I am here"
     sender.reply({ text: "#{event['message']['text']}" })
   end
